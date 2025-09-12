b1322cc1c3b925cd231d5c6c9d0b5be7e213fef7
User <user@example.com>
2025-09-12 14:55:31 +0700

    Update log
    Signed-off-by: User <user@example.com>

--- lightrag/operate.py
+++ lightrag/operate.py
@@ -2287,7 +2287,7 @@ async def _build_query_context(
     tmp_ids = []
     for tmp in text_units_context:
         content = tmp['content']
-        tmp_ids.append(c["productId"] for c in content)
+        tmp_ids.extend([c["productId"] for c in content])
     ids = ",".join(i for i in tmp_ids)
     logger.info(f"1111111111111111: IDs : {ids}")
 
